713184387f8f098efb3f9c865519efaebbca4497
panjiswt <34529676+panjiswt@users.noreply.github.com>
2018-07-03 21:24:33 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 ﻿exports.dbConfig = {
     user: "itspv",
     password: "supervisor",
-    // server: "223.25.99.220",
-    server: "192.168.18.200", 
+    server: "223.25.99.220",
+    //server: "192.168.18.200", 
     // database: "kinosentraacc",
     port: 1433,
     pool: {
