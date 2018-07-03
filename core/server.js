9235b7e046d290fcc8517745b926f07d8aebf291
panjiswt <34529676+panjiswt@users.noreply.github.com>
2018-07-03 20:56:21 +0700

    Add files via upload

---
+++ core/server.js
@@ -0,0 +1,106 @@
+﻿var http = require("http");
+var url  = require("url");
+var fs = require('fs');
+var httpMsgs = require("./httpMsgs");
+var settings = require("../settings");
+var emp = require("../controllers/employee");
+var mgl = require("../controllers/mastergl");
+var masst = require("../controllers/masterassets");
+var mcatasst = require("../controllers/mastercatassets");
+
+http.createServer(function (req, resp) {
+    var path  = url.parse(req.url).pathname;
+	var verb  = req.method;
+	console.log(verb + ": " + path);
+    resp.setHeader('Access-Control-Allow-Origin', '*');
+    
+    switch (req.method) {
+        case "GET":
+            if (req.url === "/") {
+                httpMsgs.showHome(req, resp);
+            }
+            else if (req.url === "/employees") {
+                emp.getList(req, resp);
+            }
+            else if (req.url === "/mastergl") {
+                mgl.getList(req, resp);
+            }
+            else if (req.url === "/assets") {
+                masst.getList(req, resp);
+            }
+            else if (req.url === "/categories") {
+                mcatasst.getList(req, resp);
+            }
+            else {
+                var empnoPatt = "[0-9]+";
+                var patt = new RegExp("/employees/" + empnoPatt);
+                if (patt.test(req.url)) {
+                    patt = new RegExp(empnoPatt);
+                    var empno = patt.exec(req.url);
+                    emp.get(req, resp, empno);
+                }
+                else {
+                    httpMsgs.show404(req, resp);
+                    // console.log(resp);
+                }                
+            }
+            break;
+        case "POST":
+            if (req.url === "/employees") {
+                var reqBody = '';
+                req.on('data', function (data) {
+                    reqBody += data;
+                    if (reqBody.length > 1e7) { //10MB
+                        httpMsgs.show413(req, resp);
+                    }
+                });
+                req.on('end', function () {                    
+                    emp.add(req, resp, reqBody);
+                });
+            }
+            else {
+                httpMsgs.show404(req, resp);
+            }
+            break;
+        case "PUT":
+            if (req.url === "/employees") {
+                var reqBody = '';
+                req.on('data', function (data) {
+                    reqBody += data;
+                    if (reqBody.length > 1e7) { //10MB
+                        httpMsgs.show413(req, resp);
+                    }
+                });
+                req.on('end', function () {
+                    emp.update(req, resp, reqBody);
+                });
+            }
+            else {
+                httpMsgs.show404(req, resp);
+            }
+            break;
+        case "DELETE":
+            if (req.url === "/employees") {
+                var reqBody = '';
+                req.on('data', function (data) {
+                    reqBody += data;
+                    if (reqBody.length > 1e7) { //10MB
+                        httpMsgs.show413(req, resp);
+                    }
+                });
+                req.on('end', function () {
+                    emp.delete(req, resp, reqBody);
+                });
+            }
+            else {
+                httpMsgs.show404(req, resp);
+            }
+            break;
+        default:
+            httpMsgs.show405(req, resp);
+            break;
+    }
+// }
+}).listen(settings.webPort, function () {
+    console.log("Started listening at: " + settings.webPort);
+});
---
+++ settings.js
@@ -0,0 +1,20 @@
+﻿exports.dbConfig = {
+    user: "itspv",
+    password: "supervisor",
+    // server: "223.25.99.220",
+    server: "192.168.18.200", 
+    // database: "kinosentraacc",
+    port: 1433,
+    pool: {
+        // max: 9999,
+        min: 10,
+        idleTimeoutMillis: 3000
+    }
+};
+
+exports.webPort = 9000;
+
+exports.httpMsgsFormat = "JSON";
+// exports.httpMsgsFormat = "XML";
+// exports.httpMsgsFormat = "HTML";
+
